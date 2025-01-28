879da808bf416b07a88f9ef7e1a1b9bd49678121
LuisSilva7 <lmpsilva7@gmail.com>
2025-01-28 03:38:54 +0000

    add restore cursor position

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -5,3 +5,14 @@ vim.cmd [[
         autocmd FileType java lua require'config.jdtls'.setup_jdtls()
     augroup end
 ]]
+
+-- Restore cursor position when reopening files
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line, col = mark[1], mark[2]
+    if line > 0 and line <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, { line, col })
+    end
+  end,
+})
