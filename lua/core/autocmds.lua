9a9958be9899c80f6c31f09f6e42fe71ffb32a7e
LuisSilva7 <lmpsilva7@gmail.com>
2025-01-28 04:07:50 +0000

    fix comments

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -16,3 +16,9 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = function()
+    vim.opt.formatoptions = vim.opt.formatoptions - { 'c', 'r', 'o' }
+  end,
+})
