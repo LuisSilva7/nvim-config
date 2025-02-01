909ecbea8e96ad835114e5ad6a8f7feabca953b6
LuisSilva7 <lmpsilva7@gmail.com>
2025-02-01 21:20:14 +0000

    reafctor treesitter and added autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,49 +1,46 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  opts = {
-    ensure_installed = {
-      'lua',
-      'python',
-      'javascript',
-      'typescript',
-      'vimdoc',
-      'vim',
-      'regex',
-      'terraform',
-      'sql',
-      'dockerfile',
-      'toml',
-      'json',
-      'java',
-      'groovy',
-      'go',
-      'gitignore',
-      'graphql',
-      'yaml',
-      'make',
-      'cmake',
-      'markdown',
-      'markdown_inline',
-      'bash',
-      'tsx',
-      'css',
-      'html',
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        "windwp/nvim-ts-autotag"
     },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-    textobjects = {
-      select = {
-        enable = false,
-      },
-    },
-  },
+    build = ':TSUpdate',
+    config = function()
+        local ts_config = require("nvim-treesitter.configs")
+
+        ts_config.setup({
+            ensure_installed = {
+              'lua',
+              'python',
+              'javascript',
+              'typescript',
+              'vimdoc',
+              'vim',
+              'regex',
+              'terraform',
+              'sql',
+              'dockerfile',
+              'toml',
+              'json',
+              'java',
+              'groovy',
+              'go',
+              'gitignore',
+              'graphql',
+              'yaml',
+              'make',
+              'cmake',
+              'markdown',
+              'markdown_inline',
+              'bash',
+              'tsx',
+              'css',
+              'html',
+            },
+            highlight = {enable = true},
+
+            autotag = {
+                enable = true
+            }
+        })
+    end
 }
