34631804575853316efbb4747e43a85072f0f10c
LuisSilva7 <lmpsilva7@gmail.com>
2025-02-02 01:40:06 +0000

    fix

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -8,23 +8,33 @@ return {
     'tpope/vim-sleuth',
   },
   {
-    -- Powerful Git integration for Vim
-    'tpope/vim-fugitive',
-  },
-  {
-    -- GitHub integration for vim-fugitive
-    'tpope/vim-rhubarb',
-  },
-  {
     -- Hints keybinds
     'folke/which-key.nvim',
   },
   {
-    -- Autoclose parentheses, brackets, quotes, etc.
     'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-    opts = {},
+    event = { 'InsertEnter' },
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+    },
+    config = function()
+      -- Call the autopairs setup function to configure how we want autopairs to work
+      require('nvim-autopairs').setup {
+        check_ts = true,
+        ts_config = {
+          lua = { 'string' },
+          javascript = { 'template_string' },
+          java = false,
+        },
+      }
+
+      -- Get access to auto pairs completion and cmp plugins
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+
+      -- Whenever we accept a choice from an autocompletion, make sure that any pairs are automatically closed
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
   },
   {
     -- Highlight todo, notes, etc in comments
