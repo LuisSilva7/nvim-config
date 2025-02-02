761b06e7ad21dd5838be1f219f77b5ead8601467
LuisSilva7 <lmpsilva7@gmail.com>
2025-02-02 01:13:12 +0000

    refactor autoformatting

--- lua/plugins/autoformatting.lua
+++ lua/plugins/autoformatting.lua
@@ -43,5 +43,8 @@ return {
       },
       automatic_installation = true,
     }
+
+    -- set up a vim motion for <Space> + c + f to automatically format our code based on which langauge server is active
+    vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = '[C]ode [F]ormat' })
   end,
 }
